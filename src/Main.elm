4b78e0226aca293918ce35ca5fb61371d2cc7d96
十界 <sonne@asaki.me>
2016-12-22 02:09:16 +0800

    create hello world

--- src/Main.elm
+++ src/Main.elm
@@ -1,50 +1,5 @@
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
+import Html exposing (text)
 
--- component import example
-import Components.Hello exposing ( hello )
-
-
--- APP
-main : Program Never Int Msg
+main: Html.Html text
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
-
-
--- MODEL
-type alias Model = Int
-
-model : number
-model = 0
-
-
--- UPDATE
-type Msg = NoOp | Increment
-
-update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
-
-
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
-view : Model -> Html Msg
-view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
-        ]
-      ]
-    ]
-  ]
+    text "Hello World!"
