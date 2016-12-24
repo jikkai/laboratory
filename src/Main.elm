a3a858a263f1d7cf2f1f3c40e39e8e16a93ca50c
十界 <sonne@asaki.me>
2016-12-24 23:31:01 +0800

    🌚

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,11 @@
 import Html exposing (text)
 
+names: List String
+-- names = ["Alice", "Bob", "Chuck"]
+names = []
+
+appendString: List String -> String -> String -> Html.Html text
+appendString a b c = if List.isEmpty a then text (b ++ c) else text "failed"
+
 main: Html.Html text
-main =
-    text "Hello World!"
+main = appendString names "hello world" "!"
